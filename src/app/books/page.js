ec70a16f86a185f8d61737c00978bc77212d411b
Prashant <chauhanprashant480@gmail.com>
2024-12-13 12:19:18 +0530

    create button added to list

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -29,25 +29,36 @@ export default async function Page() {
     console.log("book fetched data", data);
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
-            {data.books.map((book) => (
-                <div
-                    key={book.id}
-                    className="bg-white shadow-md rounded-lg p-4 border border-gray-200"
+        <div className="p-4">
+            <div className="mb-2 flex justify-end">
+                <button
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    // onClick={handleCreate}
                 >
-                    <Image
-                        src={book.avatar}
-                        alt={book.title}
-                        className="w-20 h-20 rounded-full mx-auto"
-                    />
-                    <h3 className="text-lg font-semibold text-center mt-4">
-                        {book.title}
-                    </h3>
-                    <p className="text-gray-600 text-center">
-                        {book.description}
-                    </p>
-                </div>
-            ))}
+                    + Create
+                </button>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {data.books.map((book) => (
+                    <div
+                        key={book.id}
+                        className="bg-white shadow-md rounded-lg p-4 border border-gray-200"
+                    >
+                        <Image
+                            src={book.avatar}
+                            alt={book.title}
+                            className="w-20 h-20 rounded-full mx-auto"
+                        />
+                        <h3 className="text-lg font-semibold text-center mt-4">
+                            {book.title}
+                        </h3>
+                        <p className="text-gray-600 text-center">
+                            {book.description}
+                        </p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
