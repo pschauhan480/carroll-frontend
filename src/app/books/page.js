5559f199c4df85d18b912c32b8c7c8ad4ca2f495
Prashant <chauhanprashant480@gmail.com>
2024-12-13 11:37:51 +0530

    apollo client and get books query added

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,3 +1,15 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_BOOKS = gql`
+    query Books {
+        books {
+            title
+            description
+            published_date
+        }
+    }
+`;
+
 export default async function Page() {
-  return <h1>Nested Page!!</h1>;
+    return <h1>Nested Page!!</h1>;
 }
